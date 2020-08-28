fd1754a1d4b50a4b32154a629891931207cb9797
Parsath <bilel.taktak@esprit.tn>
2020-08-28 17:46:44 +0100

    Submit without refresh

--- login.php
+++ login.php
@@ -9,10 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <link href="style/style.css" rel="stylesheet" media="all" type="text/css">
+    <script src="js/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link href="style/style.css" rel="stylesheet" media="all" type="text/css">
+    <script src="js/functions.js" type="text/javascript"></script>
     <title>Login</title>
 </head>
 
@@ -20,21 +21,25 @@
 
     <?php include('nav.php');?>
 
-    <div class="container mt-5">
-        <form>
+    <div class="container mt-5 contact_form">
+        <form name="contact" action="">
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="emailAdress">Email address</label>
-                    <input type="email" class="form-control" id="emailAdress" aria-describedby="emailHelp" placeholder="Enter email">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <label class="error" for="email" id="email_error">This field is required.</label>
                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
+
                 <div class="form-group col-md-6">
                     <label for="name">Name</label>
                     <input type="text" class="form-control" id="name" placeholder="Your nickname/name">
+                    <label class="error" for="name" id="name_error">This field is required.</label>
                 </div>
+
             </div>
             <div class="row justify-content-center"> 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" id="login_submit" class="btn btn-primary button" value="Send">Submit</button>
             </div>
         </form>
     </div>
--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light d-print"> 
         <a class="navbar-brand" href="https://www.facebook.com/bilel.taktak.5/" target="_blank">
-            <img class="brand-image" src="brand.jpg" alt="my pp"/>
+            <img class="brand-image" src="images/brand.jpg" alt="my pp"/>
         </a>
         <div class="navbar-nav nav-links">
             <a class="nav-item nav-link ml-5 my-item" href="index.php">Home</a>
